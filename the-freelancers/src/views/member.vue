20bb67d3456a5456a7e16d136ac2386463db28af
4l1c30zz <4l1c30zz@gmail.com>
2020-07-03 18:32:17 +0300

    skeleton complited

--- the-freelancers/src/views/member.vue
+++ the-freelancers/src/views/member.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="single-member">
+    <ul
+    v-if="member.socialMedia"
+     class="socialSideBar">
+    <li
+    v-if="member.socialMedia.email"
+    >
+    {{member.socialMedia.email}}
+    </li>
+        <li
+    v-if="member.socialMedia.website"
+    >
+    {{member.socialMedia.website}}
+    </li>
+        <li
+    v-if="member.socialMedia.facebook"
+    >
+    {{member.socialMedia.facebook}}
+    </li>
+        <li
+    v-if="member.socialMedia.linkedIn"
+    >
+    {{member.socialMedia.linkedIn}}
+    </li>
+        <li
+    v-if="member.socialMedia.instagram"
+    >
+    {{member.socialMedia.instagram}}
+    </li>
+            <li
+    v-if="member.socialMedia.telegram"
+    >
+    {{member.socialMedia.telegram}}
+    </li>
+            <li
+    v-if="member.socialMedia.github"
+    >
+    {{member.socialMedia.github}}
+    </li>
+      </ul>
     <div class="back pixel green">
       <router-link to="/" class="link pixel mark_block_wrap">
         <span class="word">
@@ -26,10 +65,15 @@
         class="inner"
       />
     </div>
-    <div v-if="member.portfolio" class="member_portfolio block">
-      {{ member.portfolio | portfolioFetch }}
-    </div>
+ <div v-if="member.portfolio"
+     :inner-html.prop="member.portfolio | portfolioFetch"
+     class="member_portfolio block">
+       
+    </div> 
+ 
+
   </div>
+  
 </template>
 
 <script>
@@ -50,7 +94,8 @@ export default {
           {
             id: null
           }
-        ]
+        ],
+
       },
 
       api_url: process.env.VUE_APP_STRAPI_API_URL,
@@ -133,30 +178,51 @@ export default {
       }
       return positionsHtml;
     },
+
     portfolioFetch: function(value) {
+     let api_url = process.env.VUE_APP_STRAPI_API_URL;
       let portfolioArr = value;
       let portfolioArrValues = portfolioArr.values();
+     
+      let full_html = '';
+      
+  
       for (var i of portfolioArrValues) {
+
         let portofiloObjectsVals = Object.values(i);
         let workTitle = portofiloObjectsVals[1];
-        console.log(workTitle + "/workTitle");
-        let externalLink = portofiloObjectsVals[2];
-        console.log(externalLink + "/externalLink");
-        let youTubeLink = portofiloObjectsVals[3];
-        console.log(youTubeLink + "/youTubeLink");
-        let imgArr = portofiloObjectsVals[4];
+full_html += "<div>";
+          full_html += "<h3>" + workTitle + "</h3>";
 
+        let externalLink = portofiloObjectsVals[2];
+        let youTubeLink = portofiloObjectsVals[3];
+        if ( youTubeLink != null) {
+        full_html += "<div class ='youtube'>" + youTubeLink+ "</div>";
+        }
+        let imgArr = portofiloObjectsVals[4];
         let imgArrValues = imgArr.values();
+
         for (var x of imgArrValues) {
           let imgArrObjVal = Object.values(x);
           let imageUrl = imgArrObjVal[1];
-          console.log(imageUrl + "/imageUrl");
+           let imageAlt = imgArrObjVal[3];
+             if ( imageUrl != null) {
+          full_html += "<img src='"+api_url + imageUrl + "' alt='"+ imageAlt + "'/>" ;
+             }
           let imageDesc = imgArrObjVal[2];
-          console.log(imageDesc + "/imageDesc");
-          let imageAlt = imgArrObjVal[3];
-          console.log(imageAlt + "/imageAlt");
+           full_html += "<p>" + imageDesc + "</p>";
+
         }
+               if ( externalLink != null) {
+         full_html += "<a href='" + externalLink + "'/>external</a>";
+        }
+        full_html += "</div>";
+            //  wrappedFullHtml += "<div class ='contWrap'>" + full_html + "</div>"; 
+
+
       }
+     
+      return full_html;
     }
   }
 };
@@ -203,5 +269,26 @@ export default {
       }
     }
   }
+}
+.position > span{
+  display: block;
+  text-align: center;
+}
+.member_portfolio {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  > div{
+    flex-basis: 33.33%;
+    padding: 20px;
+  }
+}
+.socialSideBar{
+  position: absolute;
+  right: 0;
+  top: 50%;
+  background: color(_black);
+  color: color(_pink);
+  padding: 20px 10px;
 }
 </style>
