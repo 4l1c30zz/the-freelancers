7ffa2cf3a56542802bd0b77859b718dc1837d47a
4l1c30zz <4l1c30zz@gmail.com>
2020-07-09 14:50:03 +0300

    email tooltip with copy added

--- the-freelancers/src/components/socialMemberBar.vue
+++ the-freelancers/src/components/socialMemberBar.vue
@@ -1,7 +1,10 @@
 <template>
   <ul v-if="socialMedia" class="socialSideBar">
-    <li v-if="socialMedia.email">
-      <a :href="socialMedia.email" target="_blank">
+    <li @click="show_tooltip" class="socialMedia__email" v-if="socialMedia.email">
+      <a>
+        <span @click="copy_text" class="tooltip">
+          {{socialMedia.email}}
+        </span>
         <font-awesome-icon icon="at" size="lg" />
       </a>
     </li>
@@ -50,6 +53,39 @@ export default {
   props: {
     socialMedia: Object
   },
+  methods:{
+    show_tooltip: function(event){
+      let targParent = event.target.closest('a');
+     // let tooltip = targParent.find('span');
+      var tooltip = targParent.querySelector('span');
+     tooltip.classList.toggle("a");
+    },
+    copy_text: function(event) {
+        console.log("text copy called");
+         let targ = event.target;
+               console.log(event.target);
+
+        let textToCopyVal = targ.innerHTML;
+          let el = document.createElement('textarea');
+          el.value = textToCopyVal;
+          el.setAttribute('readonly', '');
+          el.style.position = 'absolute';
+          el.style.left = '-300vw';
+          document.body.appendChild(el);
+          el.select();
+          document.execCommand('copy');
+          document.body.removeChild(el);
+          let message = document.createElement('div');
+          message.classList.add("message");
+          message.innerHTML = "text coppied to clipboard!";
+          document.body.appendChild(message);
+              setTimeout(function() {
+                message.remove();
+              }, 3000);
+          //messageElem = document.querySelector(".message");
+
+    }
+  },
   computed: {
     socialMediaCount() {
       return Math.ceil(this.socialMedia.length);
@@ -63,6 +99,15 @@ export default {
 <style lang="scss">
 @import "@/scss/_variables.scss";
 @import "@/scss/_functions.scss";
+.message{
+  position: fixed;
+  top:50%;
+  background: color(_black);
+  color: color(_green);
+  padding: 10px;
+  right: 10%;
+  font-size: 50px;
+}
 .member1 {
   .socialSideBar li {
     color: color(_pink);
@@ -102,6 +147,22 @@ export default {
   > li {
     transition: all 0.3s ease;
     color: color(_pink);
+    position: relative;
+    .tooltip{
+      display: block;
+      background: color(_black);
+      position: absolute;
+      right: 35px;
+      padding: 0;
+      top: -5px;
+      transition: all ease 0.3s;
+      height: 0;
+      overflow: hidden;
+      &.a{
+        height: auto;
+        padding: 5px;
+      }
+    }
     &:hover {
       transform: scale(1.2);
     }
